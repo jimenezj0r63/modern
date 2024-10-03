8e67b4d651481e6d8a7ce99fa8b828436ee7f1fe
jorgejimenezstack <jorgejimenez@stack-ai.app>
2024-10-03 14:51:15 -0600

    cambios minimos

--- data/index.ts
+++ data/index.ts
@@ -3,7 +3,7 @@ export const navItems = [
     { name: "Experiencia", link: "#experience" },
     { name: "Projectos", link: "#projects" },
     { name: "Nosotros", link: "#approach" },
-    { name: "Galleria", link: "#gallery" },
+    { name: "Galeria", link: "#gallery" },
 ];
 
 export const projects = [
